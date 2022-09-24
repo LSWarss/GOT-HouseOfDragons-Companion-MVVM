ff289198f2430fd419dbdf7f92e60409879fed19
LSWarss <ls.warss98@gmail.com>
2022-09-24 12:00:22 +0200

    Initial Commit

---
+++ GOT-HouseOfDragons-Companion-MVVM/GOT_HouseOfDragons_Companion_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOT_HouseOfDragons_Companion_MVVMApp.swift
+//  GOT-HouseOfDragons-Companion-MVVM
+//
+//  Created by Łukasz Stachnik on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GOT_HouseOfDragons_Companion_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
